0acafbf6b9ecfd8b8a983eb0e5f660ffe95fe6b2
hosein <h.montazer@dariahamrah.ir>
2025-02-28 17:57:57 +0330

    show calender menu

--- utils.py
+++ utils.py
@@ -5,17 +5,23 @@ from config import BASE_URL
 
 # Load user tokens from file
 def load_tokens():
-    TOKEN_FILE = "user_tokens.json"
-    if os.path.exists(TOKEN_FILE):
-        with open(TOKEN_FILE, "r") as f:
-            return json.load(f)
-    return {}
+    """Load user tokens from JSON file."""
+    try:
+        if os.path.exists('user_tokens.json') and os.path.getsize('user_tokens.json') > 0:
+            with open('user_tokens.json', 'r') as f:
+                return json.load(f)
+        else:
+            # If file doesn't exist or is empty, return empty dict
+            return {}
+    except json.JSONDecodeError:
+        # If JSON is invalid, return empty dict
+        return {}
 
 # Save user tokens to file
 def save_tokens(user_tokens):
-    TOKEN_FILE = "user_tokens.json"
-    with open(TOKEN_FILE, "w") as f:
-        json.dump(user_tokens, f)
+    """Save user tokens to JSON file."""
+    with open('user_tokens.json', 'w') as f:
+        json.dump(user_tokens, f, indent=4)
 
 # Refresh token using the refresh token
 async def refresh_token(chat_id, user_tokens):
