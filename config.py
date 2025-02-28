4640882bfebadc130db78ef1b8fa1bbff9604436
hosein <h.montazer@dariahamrah.ir>
2025-02-28 17:59:57 +0330

    show calender menu

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 TELEGRAM_BOT_TOKEN = "7527376290:AAFdYSSYJfOj1hZNsxM92U3v64yzfa3gd3U"
-BASE_URL = "http://46.245.89.2:8111"
+BASE_URL = "https://api-period.shirpala.ir"
 TOKEN_FILE = "user_tokens.json"
 
